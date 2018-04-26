1e91f4bd18d0ae6d3a2327fd343b0fb48faac8d2
Davidpa78 <davidpa7899@gmail.com>
2018-04-26 14:23:57 +0200

    Pilas

---
+++ 16_pilas/pilas.cpp
@@ -0,0 +1,37 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 0x100
+
+typedef struct{
+    int data[N];
+    int cima;
+}Pilila;
+
+void push (int nuevodato,Pilila*lapila){
+    lapila->data[lapila->cima] = nuevodato;
+    printf("--> %6i\n",lapila->data[lapila->cima]);
+    lapila->cima++;
+}
+
+int pop(Pilila *lapila){
+    lapila->cima--;
+    printf("<-- %5i\n",lapila->data[lapila->cima]);
+    return lapila->data[lapila->cima];
+}
+
+
+int main(){
+
+    Pilila datosnumericos;
+     
+    push(6,&datosnumericos);
+    push(5,&datosnumericos);
+    push(4,&datosnumericos);
+    push(-10,&datosnumericos);
+    pop(&datosnumericos);
+    pop(&datosnumericos);
+
+	return EXIT_SUCCESS;
+}
